34d608ccce816fb6ba38fffd38f24327d2cf5cfa
nickdg <delgrosso@bio.lmu.de>
2017-04-24 23:13:08 +0200

    Added some of my own blank pages, using their template file as a beginning.

--- vr_exp2_liveresults.py
+++ vr_exp2_liveresults.py
@@ -5,13 +5,26 @@ from flask import Flask, render_template
 app = Flask(__name__, static_folder='static', static_path='')
 
 
-
+#
 @app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/cliff.html')
+def cliff():
+    return render_template('cliff.html', title='Virtual Cliff Experiment')
 
-@app.route('/<path>')
+
+@app.route('/wall.html')
+def wall():
+    return render_template('wall.html', title='Virtual Wall Avoidance Experiment')
+
+
+@app.route('/objectexp.html')
+def objectexp():
+    return render_template('objectexp.html', title='Virtual Object Exploration Experiment')
+
+
+@app.route('/<string:path>')
 def catchall_path(path):
+    # return path
     return render_template(path)
 
 
